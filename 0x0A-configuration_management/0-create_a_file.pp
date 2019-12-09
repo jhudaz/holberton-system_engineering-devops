69c30f5d6e0a41ebec1f146bc8af5391cf240cb5
jandres <jandres.vr@hotmail.com>
2019-12-09 12:22:31 -0500

    update: puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file {'holberton':
-	content=>'I love Puppet',
-	path=>'/tmp/holberton',
-	mode=>'0744',
-	owner=>'www-data',
-	group=>'www-data'
+file {'create_a_file':
+content =>'I love Puppet',
+path    =>'/tmp/holberton',
+mode    =>'0744',
+owner   =>'www-data',
+group   =>'www-data'
 }
