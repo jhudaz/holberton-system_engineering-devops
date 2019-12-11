740bea88db0285abb5f6d4df34447f960714eede
jandres <jandres.vr@hotmail.com>
2019-12-10 23:16:08 -0500

    fix: path changed

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 include stdlib
 file_line { 'private keyphers':
 ensure => present,
-path   => '/etc/ssh/sshd_config',
+path   => '/etc/ssh/ssh_config',
 line   => 'IdentityFile ~/.ssh/holberton',
 }
 file_line { 'refuse authenticate using a password':
 ensure => present,
-path   => '/etc/ssh/sshd_config',
+path   => '/etc/ssh/ssh_config',
 line   => 'PasswordAuthentication no',
 }
