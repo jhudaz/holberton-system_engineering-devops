ad4cc2c4e06a6602a2af2e61a2bc5d959e5f41f9
jaime <jandres.vr@hotmail.com>
2020-02-06 13:29:18 -0500

    feature: top 10 subreddits

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+from requests import get
 
+
+def top_ten(subreddit):
+    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
+    user_agent = "application/x-www-form-urlencoded"
+    response = get(url, headers={'User-Agent': user_agent})
+
+    if response.status_code == 200:
+        data = response.json()
+        top = data.get('data').get('children')
+        for new in top:
+            title = new.get('data').get('title')
+            print(title)
+    else:
+        print('None')
