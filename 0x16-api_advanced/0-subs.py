4f9250f75ea9a475eed91dd72cf24c789cbcad60
jaime <jandres.vr@hotmail.com>
2020-02-06 11:55:21 -0500

    feature: consume reddit api to get the subscribers from a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+from requests import get
 
+
+def number_of_subscribers(subreddit):
+    """subcribers from a subreddit"""
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    user_agent = "application/x-www-form-urlencoded"
+    response = get(url, headers={'User-Agent': user_agent})
+
+    if response.status_code == 200:
+
+        json = response.json()
+        data = json.get('data').get('subscribers')
+        return int(data)
+    else:
+        return 0
