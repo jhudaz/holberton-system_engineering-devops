6f25faf030b068d598ee918338c3c575417412c4
jaime <jandres.vr@hotmail.com>
2020-02-07 00:31:59 -0500

    feature: changes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,11 +3,14 @@ from requests import get
 
 
 def recurse(subreddit, hot_list=[], after="null"):
-    url = 'https://api.reddit.com/r/{}?sort=hot?after={}&limit=100'.format(
+    url = 'https://api.reddit.com/r/{}?sort=hot&limit=100&after={}'.format(
         subreddit, after)
     user_agent = "application/x-www-form-urlencoded"
-    response = get(url, headers={'User-Agent': user_agent}).json()
-    data = response.get('data')
+    response = get(url, headers={'User-Agent': user_agent})
+    if response.status_code != 200:
+        return None
+    json = response.json()
+    data = json.get('data')
     after = data.get('after')
 
     if not after:
