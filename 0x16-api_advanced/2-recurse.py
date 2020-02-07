6d350de73ac780fcca0d3feafced2056e39abe67
jaime <jandres.vr@hotmail.com>
2020-02-07 00:14:37 -0500

    feature: changes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 from requests import get
 
-def recurse(subreddit, hot_list=[]):
-    url='https://api.reddit.com/r/{}?sort=hot&limit=100'.format(subreddit)
+
+def recurse(subreddit, hot_list=[], after="null"):
+    url = 'https://api.reddit.com/r/{}?sort=hot?after={}&limit=100'.format(
+        subreddit, after)
     user_agent = "application/x-www-form-urlencoded"
     response = get(url, headers={'User-Agent': user_agent}).json()
-
     data = response.get('data')
-
     after = data.get('after')
 
-
-    if(after is None):
+    if not after:
         return hot_list
     else:
-        articles =  data.get('children')
-        for article in  articles:
-            title = article.get('data').get('title')
-            hot_list.append(title)
-        
-        recurse(subreddit, hot_list)
-    
-
-
-
-
+        articles = data.get('children')
+        hot_list = hot_list + articles
+        return recurse(subreddit, hot_list, after)
